24349dbc923d39c33229e2fbed3b4a528f2ab63a
Gregory Romé <gregory.rome@gmail.com>
2015-04-10 18:07:00 +0200

    config: setup generators for the engine

--- lib/engine_template/engine.rb
+++ lib/engine_template/engine.rb
@@ -1,5 +1,13 @@
 module EngineTemplate
   class Engine < ::Rails::Engine
     isolate_namespace EngineTemplate
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: true, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'test/factories'
+      g.factory_girl true
+      g.assets false
+      g.helper false
+    end
   end
 end
