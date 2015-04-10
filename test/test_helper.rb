a3621d371f850ad60b086191f9e4cd4e66111300
Gregory Romé <gregory.rome@gmail.com>
2015-04-10 18:07:29 +0200

    test: adds factory_girl support to helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,12 @@ require File.expand_path('../../test/dummy/config/environment.rb',  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require 'rails/test_help'
+
 require 'minitest/rails'
 require 'minitest/rails/capybara'
 require 'minitest/pride'
+
+require 'factory_girl_rails'
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
