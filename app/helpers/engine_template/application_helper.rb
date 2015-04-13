95a320d9dee385031a5bbf4ccc95fcc90efcb992
Gregory Romé <gregory.rome@gmail.com>
2015-04-13 11:33:55 +0200

    adds helper to check if a menu item is active

--- app/helpers/engine_template/application_helper.rb
+++ app/helpers/engine_template/application_helper.rb
@@ -1,4 +1,13 @@
 module EngineTemplate
   module ApplicationHelper
+    def menu_item_is_active(options={})
+      active = nil
+      if controller_name == options[:controller]
+        if action_name == options[:action]
+          active = 'active'
+        end
+      end
+      active
+    end
   end
 end
