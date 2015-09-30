080f0ab8893d167a438cb698c9b2fee0c7a9a7ff
Gregory Romé <gregory.rome@gmail.com>
2015-09-30 21:43:02 +0200

    test: adds devise user for testing

---
+++ test/dummy/app/models/user.rb
@@ -0,0 +1,52 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+    # -----------------------------------------------------
+    # Constants
+
+    # -----------------------------------------------------
+    # Associations
+
+    # -----------------------------------------------------
+    # Validations
+
+    # -----------------------------------------------------
+    # Hooks
+
+    # -----------------------------------------------------
+    # Instance methods
+
+
+    # -----------------------------------------------------
+    # Class methods
+
+
+    # -----------------------------------------------------
+    # Private methods
+    private
+
+    # -----------------------------------------------------
+    # rails_admin configuration
+    rails_admin do
+      list do
+      end
+      show do
+      end
+      edit do
+      end
+
+      create do
+      end
+
+      update do
+      end
+
+      nested do
+      end
+
+      modal do
+      end
+    end
+  end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/home'
 
   mount EngineTemplate::Engine => '/engine_template'
