a23a77c62a672072f8a1118dbbd037a3c5f8d388
Gregory Romé <gregory.rome@gmail.com>
2015-10-06 22:02:04 +0200

    adds configuration

--- lib/engine_template.rb
+++ lib/engine_template.rb
@@ -1,2 +1,20 @@
 require 'engine_template/engine'
 
+module EngineTemplate
+  @@configured = false
+  @@user_class = 'User'
+
+  def self.configured? #:nodoc:
+    @@configured
+  end
+
+  def self.user_class
+    @@user_class.constantize
+  end
+
+  def self.config
+    @@configured = true
+    yield self
+  end
+end
+
