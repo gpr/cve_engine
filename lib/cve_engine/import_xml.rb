565df9198ba1d068b295bf70250d37a753ecec98
Gregory Romé <gregory.rome@gmail.com>
2016-03-19 14:53:47 +0100

    task: fixes import xml task

--- lib/cve_engine/import_xml.rb
+++ lib/cve_engine/import_xml.rb
@@ -4,7 +4,7 @@ module CveEngine
   def self.import_xml(xml_filename)
     Nokogiri::XML::Reader(File.open(xml_filename)).each do |node|
       if node.name == 'entry' && node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
-        vuln = Vulnerability.find_or_create_from_xml(node.outer_xml)
+        vuln = CveEngine::Vulnerability.find_or_create_from_xml(node.outer_xml)
         puts "#{vuln.cve_id} has been processed"
       end
     end
--- lib/tasks/cve_engine_tasks.rake
+++ lib/tasks/cve_engine_tasks.rake
@@ -4,7 +4,7 @@ require 'cve_engine/import_xml'
 namespace :cve_engine do
 
   desc 'Import NVD CVE XML file'
-  task :import_xml, [:xml_file] do
+  task :import_xml, [:xml_file] => :environment do |task, args|
     CveEngine.import_xml(args[:xml_file])
   end
 
